033f29c31a2450a9c230c59e0fb1802e0c229631
Ivan Xu <info@chivalrystudio.com>
2021-09-02 23:22:24 -0700

    save

--- FastMultipoleMethod/Quadtree.h
+++ FastMultipoleMethod/Quadtree.h
@@ -21,7 +21,7 @@ namespace tree_helper
 	}
 
 	template <std::size_t Level>
-	using num_nodes_ = std::integral_constant<std::size_t, num_nodes(Level)>;
+	using num_nodes_ = std::integral_constant<std::size_t, num_nodes(Level - 1)>;
 }
 
 
@@ -32,14 +32,23 @@ struct tree_node
 template <std::size_t Level>
 class quadtree
 {
+	static_assert(Level <= 10, "Level should be less than 10.");
+
 public:
 	constexpr quadtree() : data_(), level_(Level)
 	{
+		for (std::size_t i = 0; i < Level; ++i)
+		{
+			num_nodes_at_level_[i] = pow(4, i);
+		}
 	}
 
 	void clear() { data_.fill(nullptr); }
 
-private:
+protected:
 	std::array<tree_node*, tree_helper::num_nodes_<Level>::value> data_;
-	std::size_t level_;  
+	std::size_t level_;
+
+private:
+	std::array<std::size_t, Level> num_nodes_at_level_;
 };
